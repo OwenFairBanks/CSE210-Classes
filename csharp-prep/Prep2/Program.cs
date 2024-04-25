a588ba236affd4eb91c021cf4a6ca2c4c0b0e943
OwenFairBanks <fai22009@byui.edu>
2024-04-25 13:11:28 -0600

    Finished main code

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,39 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("\n\n\nWhat is your percentage? ");
+        string user_input = Console.ReadLine();
+        int x = int.Parse(user_input);
+        Console.WriteLine("");
+        if ( x >= 90 )
+        {
+            Console.WriteLine("You have an A");
+        }
+        else if ( x >= 80 )
+        {
+            Console.WriteLine("You have a B");
+        }
+        else if ( x >= 70 )
+        {
+            Console.WriteLine("You have a C");
+        }
+        else if ( x >= 60 )
+        {
+            Console.WriteLine("You have a d");
+        }
+        else
+        {
+            Console.WriteLine("You have a F");
+        }
+        if (x >= 70 )
+        {
+            Console.WriteLine("Congradulations, you passed!");
+        }
+        else
+        {
+            Console.WriteLine("Sorry to inform you, but you have failed.");
+        }
+        Console.WriteLine("");
+        //Just need some spacing here lol
     }
 }
