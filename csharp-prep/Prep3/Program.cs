ec0a685ccaaea9230b967869bc40873505e97943
OwenFairBanks <fai22009@byui.edu>
2024-04-29 09:31:46 -0600

    Finished code

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Console.Write("What is the magic number? ");
+        string user_input = Console.ReadLine();
+        int key = int.Parse(user_input);
+        int guess = -1;
+        while (key != guess )
+        {
+            Console.Write("\nWhat is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+            Console.WriteLine("");
+            if ( key > guess )
+            {
+                Console.WriteLine("Higher");
+            }
+            else if ( key < guess )
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!\n");
+            }
+        }
+
     }
 }
