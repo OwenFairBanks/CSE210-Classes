590bd605bb88b4dfacaf4ab1cb34f924f9336f93
OwenFairBanks <fai22009@byui.edu>
2024-05-01 09:04:56 -0600

    Finished main goal

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,31 +1,32 @@
 using System;
 
-string play = "yes";
-while (play == "yes")
-    play = "no";
-    Console.Write("What is the magic number? ");
-    string user_input = Console.ReadLine();
-    int key = int.Parse(user_input);
-    int guess = -1;
-    int amount = 0;
-    while (key != guess)
+class Program
+{
+    static void Main(string[] args)
     {
-        Console.Write("\nWhat is your guess? ");
-        guess = int.Parse(Console.ReadLine());
-        Console.WriteLine("");
-        amount += 1;
-        if (key > guess)
+        Console.Write("What is the magic number? ");
+        string user_input = Console.ReadLine();
+        int key = int.Parse(user_input);
+        int guess = -1;
+        int amount = 0;
+        while (key != guess)
         {
-            Console.WriteLine("Higher");
-        }
-        else if (key < guess)
-        {
-            Console.WriteLine("Lower");
-        }
-        else
-        {
-            Console.WriteLine($"You guessed it!\nGuesses taken: {amount}");
+            Console.Write("\nWhat is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+            Console.WriteLine("");
+            amount += 1;
+            if (key > guess)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (key < guess)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine($"You guessed it!\nGuesses taken: {amount}");
+            }
         }
     }
-    Console.Write("Will you play again? ");
-    play = Console.ReadLine();
+}
