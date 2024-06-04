543934ce13bccaa15c18f571588a34dd31a5fe3f
OwenFairBanks <fai22009@byui.edu>
2024-06-04 17:08:15 -0600

    Completed the Main Program

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment p1 = new Assignment("Samuel Bennett", "Multiplication");
+        MathAssignment p2 = new MathAssignment("Roberto Rodriguez", "Fractions", "Section 7.3", "Problems 8-19");
+        WritingAssignment p3 = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II");
+
+        Console.WriteLine(p1.GetSummary());
+        Console.WriteLine("");
+        Console.WriteLine(p2.GetSummary());
+        Console.WriteLine(p2.GetHomework());
+        Console.WriteLine("");
+        Console.WriteLine(p3.GetSummary());
+        Console.WriteLine(p3.GetWritingInfo());
+        Console.WriteLine("");
+
     }
 }
