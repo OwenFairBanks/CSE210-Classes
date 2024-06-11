3115e0ea61ee7bb3a11e503863fa4ae707110307
OwenFairBanks <fai22009@byui.edu>
2024-06-11 14:13:31 -0600

    I believe I've finished all the programs I'll need for Timed stuff

---
+++ prove/Develop04/TimingStuff.cs
@@ -0,0 +1,50 @@
+public class TimedItems
+{
+    private int _seconds;
+    private List<string> _animation;
+
+    public TimedItems(int seconds)
+    {
+        _seconds = seconds;
+        _animation = ["/","-","\\","|"];
+    }
+
+    public DateTime TimeLimit(int seconds)
+    {
+        DateTime startTime = DateTime.Now;
+        DateTime endTime = startTime.AddSeconds(seconds);
+        return endTime;
+    }
+    public string LoadingAnimation(int seconds)
+    {
+        DateTime startTime = DateTime.Now;
+        DateTime endTime = startTime.AddSeconds(seconds);
+        int i = 0;
+        while (DateTime.Now < endTime)
+        {
+            string s = _animation[i];
+            Console.Write(s);
+            Thread.Sleep(250);
+            Console.Write("\b \b");
+            i++;
+            if (i >= _animation.Count)
+            {
+                i = 0;
+            }
+        }
+        return "";
+    }
+    public string CountdownAnimation()
+    {
+        Console.Write("You may begin in: ");
+        for (int i = 5; i > 0; i-- )
+        {
+            Console.Write(i);
+            // Console.Write(i);
+            Thread.Sleep(1000);
+            Console.Write("\b \b");
+        }
+        return "";
+    }
+
+}
