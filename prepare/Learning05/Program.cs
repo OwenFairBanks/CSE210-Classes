f1d3469f242313667e62046133b2ab46af241fb3
OwenFairBanks <fai22009@byui.edu>
2024-06-17 16:54:34 -0600

    Finished main program

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -1,9 +1,26 @@
 using System;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>();
+
+        Square s1 = new Square("Green", 5);
+        shapes.Add(s1);
+        Rectangle s2 = new Rectangle("Yellow", 6, 4);
+        shapes.Add(s2);
+        Circle s3 = new Circle("Purple", 3);
+        shapes.Add(s3);
+
+        foreach (Shape s in shapes)
+        {
+            string color = s.GetColor();
+
+            double area = s.GetArea();
+
+            Console.WriteLine($"The {color} shape has an area of {area}.");
+        }
     }
 }
